ef7bb73179b1c7220ccb9550486aca194639e87c
Enes Can Işık <enes.isik@armagroupholding.com>
2022-12-11 18:05:38 +0300

    ATMEGA32 HOME AUTOMATION

---
+++ myHomeAutomation/main.c
@@ -0,0 +1,228 @@
+/*
+ * myHomeAutomation.c
+ *
+ * Created: 30.05.2019 23:58:02
+ * Author : VO
+ */ 
+#include <util/delay.h>
+#include <string.h>
+#include <avr/sfr_defs.h>
+#include <stdio.h>
+#include <avr/io.h>
+#include "ser_il328.h"
+#define POT 500000
+void ADC_Load()
+{
+	DDRC = 0x00;	        /* Make ADC port as input */
+	//ADCSRA = 0x87;          /* Enable ADC, with freq/128  */
+	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN); 
+}
+int ADC_Read_ch(char channel)
+{
+	switch(channel)
+	{
+		 case 0:
+			 ADMUX=0x40;
+			 break;
+		 case 1:
+			 ADMUX=0x41;
+			 break;
+		 case 2:
+			 ADMUX=0x42;
+			 break;
+		 case 3:
+			 ADMUX=0x43;
+			 break;
+		 default:
+			 ADMUX=0x44;
+			 break;
+	}
+	float myValue=0;
+	for(int i=0;i<1000;i++)
+	{
+		ADCSRA |= (1<<ADSC);               /* Start ADC conversion */
+		while (!(ADCSRA & (1<<ADIF)));     /* Wait until end of conversion by polling ADC interrupt flag */
+		ADCSRA |= (1<<ADIF);               /* Clear interrupt flag */
+		_delay_us(100);                      /* Wait a little bit */
+		myValue=myValue+ADCW;                       /* Return ADC word */
+	}
+	myValue=myValue/1000;
+	return myValue;
+}
+
+int main(void)
+{
+	seri_yukle(16,9600);
+	ADC_Load();
+	DDRD=0xFF;
+	DDRB=0xFC;
+	int a,basla=0,tempperde=0;
+	float degree=0,potval1=0,potval2=0,light=0,tiktok=0;
+	char rec;
+    while (1) 
+    {
+		if(basla==1)
+		{
+			tempperde=tempperde+1;
+				if(tiktok<10)
+				{
+					if(tempperde!=2)
+						continue;
+					else
+					{
+						tempperde=0;
+						if (bit_is_clear(PIND,4))
+						{
+							tiktok++;
+							PORTD |=(1<<PIND4);
+						}
+						else
+							PORTD &= ~(1<<PIND4);
+					}
+				}
+				else
+				{
+					basla=0;
+					tiktok=0;
+				}
+		}
+		rec=al(a);
+		if(rec!=1)
+		{
+			if(rec=='d')
+			{
+				if (bit_is_clear(PINB,2))
+					PORTB |=(1<<PINB2);
+				else
+					PORTB &= ~(1<<PINB2);
+			}
+			if(rec=='e')
+			{
+				if (bit_is_clear(PINB,3))
+					PORTB |=(1<<PINB3);
+				else
+					PORTB &= ~(1<<PINB3);
+			}
+			if(rec=='f')
+			{
+				if (bit_is_clear(PINB,4))
+					PORTB |=(1<<PINB4);
+				else
+					PORTB &= ~(1<<PINB4);
+			}
+			if(rec=='q')
+			{
+				if(basla==0)
+					basla=1;
+				else
+					basla=0;
+			}
+		}
+		degree=((ADC_Read_ch(0))*3.68)/10;
+		DegreeState(degree);
+		convert2string(degree);
+		gonder('d');
+		//_delay_ms(600);
+		potval1=(double)POT/1024*ADC_Read_ch(1);
+		convert2string(potval1);
+		gonder('p');
+		if(potval1>300000 && degree>60)
+		{
+			PORTB |=(1<<PINB5);
+			gonder('y');
+		}
+		else
+			PORTB &= ~(1<<PINB5);	
+		if(potval1>400000)
+		{
+			gonder('w');
+		}
+		else
+		PORTB &= ~(1<<PINB5);
+		//_delay_ms(600);
+		potval2=(double)POT/1024*ADC_Read_ch(2);
+		if(potval2>300000)
+		{
+			PORTB |=(1<<PINB5);
+			gonder('a');
+		}
+		else
+			PORTB &= ~(1<<PINB5);
+		convert2string(potval2);
+		gonder('p');
+		gonder('l');
+		//_delay_ms(600);
+		light=ADC_Read_ch(3);
+		convert2string(light);
+		//_delay_ms(600);
+		//_delay_ms(50);
+		//celsius = (ADC_Read(0)*4.88);
+		//celsius = (celsius/10.00);
+		//_delay_ms(800);
+    }
+}
+int temp1,temp2,temp3;
+void DegreeState(float myState)
+{
+	if (myState<30)
+	{
+		temp1=1;
+		PORTD |=(1<<PIND5);
+		gonder('g');//good
+	}
+	else if(temp1==1)
+	{
+		temp1=0;
+		PORTD &= ~(1<<PIND5);
+	}
+	if ((myState>30) && (myState<40))
+	{
+		temp2=1;
+		PORTD |=(1<<PIND5);
+		PORTD |=(1<<PIND6);
+		gonder('s');
+		gonder('h');//hot
+	}
+	else if(temp2==1)
+	{
+		temp2=0;
+		PORTD &= ~(1<<PIND5);
+		PORTD &= ~(1<<PIND6);
+	}
+	if((myState>40) && (myState<60))
+	{
+		temp3=1;
+		PORTD |=(1<<PIND5);
+		PORTD |=(1<<PIND6);
+		PORTD |=(1<<PIND7);
+		gonder('v');
+		gonder('h'); //veryhot
+	}
+	else if(temp3==1)
+	{
+		temp3=0;
+		PORTD &= ~(1<<PIND5);
+		PORTD &= ~(1<<PIND6);
+		PORTD &= ~(1<<PIND7);
+	}
+	if(60<myState)
+	{
+		gonder('f');
+		//if(myDegree<=25)
+	}
+	return;
+}
+void convert2string(float convert)
+{
+	char buffer[12]={'0','0','0','0','0','0','0','0','0','0','0','0'};
+	char t[12]={'0','0','0','0','0','0','0','0','0','0','0','0'};
+	dtostrf(convert, 12, 2, t);
+	for (int i=0;i<sizeof(t);i++)
+	{
+		gonder(t[i]);
+	}
+	return;
+}
+
+
+
---
+++ myHomeAutomation/ser_il328.h
@@ -0,0 +1,106 @@
+#include <stdio.h> //standart Giriþ- Çýkýþ
+
+ #define C_DefaultDigitsToTransmit_U8          0xffu    // Will transmit the exact digits in the number
+ #define C_MaxDigitsToTransmit_U8              10u      // Max decimal/hexadecimal digits to be transmitted
+ #define C_NumOfBinDigitsToTransmit_U8         16u      // Max bits of a binary number to be transmitted
+ #define C_MaxDigitsToTransmitUsingPrintf_U8   C_DefaultDigitsToTransmit_U8 /* Max dec/hexadecimal digits to be displayed using printf */
+ /**************************************************************************************************/
+void gonder(char kar); //Veri Gönderme
+int al(char kar);   //Veri Alma
+FILE data= FDEV_SETUP_STREAM(gonder, al, _FDEV_SETUP_RW);//(gönderilen, alýnan, Etkilenen Bayrak)) / _FDEV_SETUP_RW= okuma - yazma
+
+
+
+void seri_yukle(uint16_t CPU_MHZ,uint16_t BAUD )
+{
+	int ubrr=(CPU_MHZ*1000000)/16/BAUD-1;
+	stdout=stdin=&data; //standart girdi ve ciktiyi veriler degiskenine yonlendir
+	UCSR0A=0x00; // Senkron için kullanýlýr. asenkron yaptýðým için kapalý
+	UCSR0B=0x18; // 00011000 - TXEN - PD0 pini Alýcý olur / RXEN - PD1 pini Gönderir
+	UCSR0C=0x86; // 10000110 - UCSZ0 / UCSZ1 / ursel akitf.
+	UBRR0H=0x00;
+	UBRR0L=ubrr; // Baðlantý hýzý
+}
+
+void gonder(char kar)
+{
+	//loop_until_bit_is_set(UCSRA, UDRE); // ucsra nýn 5. yazmacý olan udre
+	// 1 olduðunda, yeni veri için boþ olduðunu gösterir. eðer boþsa gönder iþlemi yapýlýr
+	while (!(UCSR0A&(1<<UDRE0)));			// 2. yol
+	
+	UDR0 = kar;							// Gönderme ve alma UDR den geçer.
+}
+
+int al(char kar)
+{
+	//loop_until_bit_is_set(UCSRA,RXC); // UCSRA içindeki 7. bitinde bulunan RXC yazmacý akitif olduðunda kullan
+	
+	while (!(UCSR0A & (1<<RXC0)));
+	return kar= UDR0;
+}
+#if ((Enable_UART_TxNumber==1) || (Enable_UART_TxFloatNumber==1) || (Enable_UART_Printf == 1))
+void UART_TxNumber(uint8_t v_numericSystem_u8, uint32_t v_number_u32, uint8_t v_numOfDigitsToTransmit_u8)
+{
+    uint8_t i=0,a[10];
+
+    if(C_BINARY_U8 == v_numericSystem_u8)
+    {
+        while(v_numOfDigitsToTransmit_u8!=0)
+        {
+            /* Start Extracting the bits from the specified bit positions.
+             Get the Acsii values of the bits and transmit */
+            i = util_GetBitStatus(v_number_u32,(v_numOfDigitsToTransmit_u8-1));
+            gonder(util_Dec2Ascii(i));
+            v_numOfDigitsToTransmit_u8--;
+        }    
+    }     
+    else if(v_number_u32==0)
+    {
+        /* If the number is zero then update the array with the same for transmitting */
+        for(i=0;((i<v_numOfDigitsToTransmit_u8) && (i<C_MaxDigitsToTransmit_U8)) ;i++)
+            gonder('0');
+    }
+    else
+    {
+        for(i=0;i<v_numOfDigitsToTransmit_u8;i++)
+        {
+            /* Continue extracting the digits from right side
+               till the Specified v_numOfDigitsToTransmit_u8 */
+            if(v_number_u32!=0)
+            {
+                /* Extract the digits from the number till it becomes zero.
+                First get the remainder and divide the number by 10 each time.
+
+                example for Decimal number:
+                If v_number_u32 = 123 then extracted remainder will be 3 and number will be 12.
+                The process continues till it becomes zero or max digits reached*/
+                a[i]=util_GetMod32(v_number_u32,v_numericSystem_u8);
+                v_number_u32=v_number_u32/v_numericSystem_u8;
+            }
+            else if( (v_numOfDigitsToTransmit_u8 == C_DefaultDigitsToTransmit_U8) ||
+                    (v_numOfDigitsToTransmit_u8 > C_MaxDigitsToTransmit_U8))
+            {
+                /* Stop the iteration if the Max number of digits are reached or
+                 the user expects exact(Default) digits in the number to be transmitted */ 
+                break;
+            }
+            else
+            {
+                /*In case user expects more digits to be transmitted than the actual digits in number,
+                  then update the remaining digits with zero.
+                Ex: v_number_u32 is 123 and user wants five digits then 00123 has to be transmitted */
+                a[i]=0;
+            }
+        }
+
+        while(i)
+        { 
+            /* Finally get the ascii values of the digits and transmit*/
+            gonder(util_Hex2Ascii(a[i-1]));
+            i--;
+        }
+    }
+
+
+}
+#endif
